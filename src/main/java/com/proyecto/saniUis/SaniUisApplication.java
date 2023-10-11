84871b45153ce6fc38c8842156daf8f247f5d93e
judamov <judamov.09@gmail.com>
2023-10-11 18:09:04 -0500

    commit inicial

---
+++ src/main/java/com/proyecto/saniUis/SaniUisApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.saniUis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaniUisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaniUisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/saniUis/SaniUisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.saniUis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaniUisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
